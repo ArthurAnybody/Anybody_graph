714477641ab5aca8b8c3c73b7f3dec96183f313a
ArthurSALLÉ <arthur.salle.1@ens.etsmtl.ca>
2025-08-05 14:06:13 -0400

    Add files via upload

---
+++ type_colors.py
@@ -0,0 +1,33 @@
+# type_colors.py
+
+# Couleurs fixes par nom exact des cas
+FIXED_TYPE_COLORS = {
+    'Référencev4': 'black',               # Cas de référence
+    'Ratio1_type_Av4': '#1f77b4',       # blue
+    'Ratio1_type_Bv4': '#ff7f0e',       # orange
+    'Ratio1_type_Cv4': '#2ca02c',       # green
+    'Ratio1_type_Dv4': '#d62728',       # red
+    'Ratio1_type_Ev4': '#9467bd',       # purple
+    
+    'Reference': 'black',               # Cas de référence
+    'Type_A': '#1f77b4',       # blue
+    'Type_B': '#ff7f0e',       # orange
+    'Type_C': '#2ca02c',       # green
+    'Type_D': '#d62728',       # red
+    'Type_E': '#9467bd', 
+    
+    
+    
+    # Tu peux ajouter d'autres types ici si besoin
+}
+
+#         '#1f77b4',  # blue
+#         '#ff7f0e',  # orange
+#         '#2ca02c',  # green
+#         '#d62728',  # red
+#         '#9467bd',  # purple
+#         '#8c564b',  # brown
+#         '#e377c2',  # pink
+#         '#7f7f7f',  # gray
+#         '#bcbd22',  # yellow-green
+#         '#17becf'   # cyan
